b97a30bec7c2cf06982917ae19aa344f69f10773
skpark33 <skpark@sqisoft.com>
2023-01-02 18:48:04 +0900

    20230102-01-skpark

---
+++ lib/pages/studio/studio_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../design_system/creta_color.dart';
+
+class LayoutConst {
+  static const double menuStickWidth = 80.0;
+  static const double menuStickIconAreaHeight = 72.0;
+  static const double menuStickIconSize = 32.0;
+
+  static Color studioBGColor = CretaColor.text[200]!;
+  static const Color menuStickBGColor = Colors.white;
+}
