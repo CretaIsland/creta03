b4e23f25c40fb0eea124cab602b216698d93e4af
Hnarae <nr.han@sqisoft.com>
2023-05-04 10:47:27 +0900

    20230504-02-nr.han define team model, team data manager

--- lib/data_io/team_manager.dart
+++ lib/data_io/team_manager.dart
@@ -1,4 +1,4 @@
-import 'package:hycop/hycop/absModel/abs_ex_model.dart';
+import 'package:hycop/hycop.dart';
 import '../model/creta_model.dart';
 import '../model/team_model.dart';
 import 'creta_manager.dart';
@@ -6,7 +6,11 @@ import 'creta_manager.dart';
 TeamManager? pageManagerHolder;
 
 class TeamManager extends CretaManager {
-  TeamManager() : super('creta_page');
+
+  List<TeamModel> teamModelList = [];
+
+
+  TeamManager() : super('creta_team');
   @override
   AbsExModel newModel(String mid) => TeamModel(mid);
 
@@ -16,4 +20,55 @@ class TeamManager extends CretaManager {
     src.copyTo(retval);
     return retval;
   }
+
+  // create team object
+  Future<TeamModel> createTeam({required String teamName, required String ownerId}) async {
+    TeamModel newTeam = TeamModel.withName(name: teamName, owner: ownerId);
+
+    await createToDB(newTeam);
+    insert(newTeam, postion: getAvailLength());
+
+    return newTeam;
+  }
+
+  // get team object
+  Future<int> getTeams({required List<String> teamMids, int limit = 99}) async {
+    int teamCount = 0;
+
+    try {
+      for(var mid in teamMids) {
+        if(await _getTeam(teamMid: mid) == 1) {
+          teamCount += 1;
+        }
+      }
+    } catch (error) {
+      logger.info("something wrong $error");
+      return 0;
+    }
+    return teamCount;
+  }
+
+  Future<int> _getTeam({required String teamMid, int limit = 99}) async {
+    startTransaction();
+
+    Map<String, QueryValue> query = {};
+    query["mid"] = QueryValue(value: teamMid);
+    query["isRemoved"] = QueryValue(value: false);
+
+    await queryFromDB(query, limit: limit);
+
+    if(modelList.isNotEmpty && !teamModelList.contains(onlyOne() as TeamModel)) {
+      teamModelList.add(onlyOne() as TeamModel);
+    }
+
+    endTransaction();
+    return modelList.length;
+  }
+
+
+
+
+
+
+
 }
--- lib/model/team_model.dart
+++ lib/model/team_model.dart
@@ -2,6 +2,7 @@
 
 //import 'dart:convert';
 
+import 'package:creta03/common/creta_utils.dart';
 import 'package:hycop/common/util/logger.dart';
 import 'package:hycop/hycop/absModel/abs_ex_model.dart';
 import 'package:hycop/hycop/enum/model_enums.dart';
@@ -9,34 +10,78 @@ import 'creta_model.dart';
 
 // ignore: camel_case_types
 class TeamModel extends CretaModel {
-  late String name;
+
+  late String name;                 // 팀의 이름
+  late String profileImg;           // 팀 프로필 사진
+  late String channelBannerImg;     // 팀 채널 배너 사진
+  late String owner;                // 팀 요금제를 가입한 사람
+  late List<String> managers;       // owner가 manager로 지정한 사람들 
+  late List<String> generalMembers;  // 일반 멤버
+  late bool isPublicProfile;        // 팀 채널 공개 여부
+  late List<String> allMembers;    // manager, member, owner 모든 value를 통합한 List (where 검색을 위한 필드)
 
   @override
   List<Object?> get props => [
         ...super.props,
         name,
+        profileImg,
+        channelBannerImg,
+        owner,
+        managers,
+        generalMembers,
+        isPublicProfile,
+        allMembers
       ];
 
-  TeamModel(String pmid) : super(pmid: pmid, type: ExModelType.user, parent: '') {
+  TeamModel(String pmid) : super(pmid: pmid, type: ExModelType.team, parent: '') {
     name = '';
+    profileImg = '';
+    channelBannerImg = '';
+    owner = '';
+    managers = [];
+    generalMembers = [];
+    isPublicProfile = true;
+    allMembers = [];
   }
 
   TeamModel.withName({
     required this.name,
-  }) : super(pmid: '', type: ExModelType.user, parent: '');
+    required this.owner,
+    this.profileImg = '',
+    this.channelBannerImg = '',
+    this.managers = const [],
+    this.generalMembers = const [],
+    this.isPublicProfile = true
+  }) : super(pmid: '', type: ExModelType.team, parent: '') {
+    allMembers = [owner, ...managers, ...generalMembers];
+  }
 
   @override
   void copyFrom(AbsExModel src, {String? newMid, String? pMid}) {
     super.copyFrom(src, newMid: newMid, pMid: pMid);
-    TeamModel srcUser = src as TeamModel;
-    name = srcUser.name;
-    logger.finest('UserCopied($mid)');
+    TeamModel srcTeam = src as TeamModel;
+    name = srcTeam.name;
+    profileImg = srcTeam.profileImg;
+    channelBannerImg = srcTeam.channelBannerImg;
+    owner = srcTeam.owner;
+    managers = [...srcTeam.managers];
+    generalMembers = [...srcTeam.generalMembers];
+    isPublicProfile = srcTeam.isPublicProfile;
+    allMembers = [srcTeam.owner, ...srcTeam.managers, ...srcTeam.generalMembers];
+    logger.finest('TeamCopied($mid)');
   }
 
   @override
   void fromMap(Map<String, dynamic> map) {
     super.fromMap(map);
     name = map["name"] ?? '';
+    profileImg = map["profileImg"];
+    channelBannerImg = map["channelBannerImg"];
+    owner = map["owner"] ?? '';
+    managers = CretaUtils.jsonStringToList(map["managers"]);
+    generalMembers = CretaUtils.jsonStringToList(map["generalMembers"]);
+    isPublicProfile = map["isPublicProfile"];
+    allMembers = CretaUtils.jsonStringToList(map["allMembers"]);
   }
 
   @override
@@ -44,6 +89,13 @@ class TeamModel extends CretaModel {
     return super.toMap()
       ..addEntries({
         "name": name,
+        "profileImg" : profileImg,
+        "channelBannerImg" : channelBannerImg,
+        "owner" : owner,
+        "managers" : CretaUtils.listToString(managers),
+        "generalMembers" : CretaUtils.listToString(generalMembers),
+        "isPublicProfile" : isPublicProfile,
+        "allMembers" : CretaUtils.listToString(allMembers)
       }.entries);
   }
 }
