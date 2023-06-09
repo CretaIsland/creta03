0d0c893c0ed2dc38ae117da7c2fb11323cc2aa88
Hnarae <nr.han@sqisoft.com>
2023-06-09 16:11:18 +0900

    20230609-02-nr.han fix problems

--- lib/model/enterprise_model.dart
+++ lib/model/enterprise_model.dart
@@ -1,6 +1,7 @@
 import 'package:creta03/model/creta_model.dart';
 import 'package:hycop/hycop/enum/model_enums.dart';
 
+// ignore: must_be_immutable
 class EnterpriseModel extends CretaModel {
 
   late String name;
