1eaa9b66ff71341142c10416d42fc41bbdb619e7
skpark33 <skpark@sqisoft.com>
2023-06-28 19:58:26 +0900

    20230628-04-skpark

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -20,7 +20,7 @@ class _IntroPageState extends State<IntroPage> {
       context: context,
       child: Center(
         child: Text(
-          "Version 0.0.6 (hycop 0.2.5)",
+          "Version 0.0.7 (hycop 0.2.5)",
           style: CretaFont.headlineLarge,
         ),
       ),
