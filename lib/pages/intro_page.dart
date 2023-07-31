da399b9d3c7490ea2fe4a475c6fe0192d2ee4391
skpark33 <skpark@sqisoft.com>
2023-07-31 12:55:47 +0900

    20230731-03-skpark

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -100,7 +100,7 @@ class _IntroPageState extends State<IntroPage> {
                 height: videoHeight,
                 child: Center(
                   child: Text(
-                    "Version 0.2.21 (hycop 0.2.15) - build 20230731-09",
+                    "Version 0.2.22 (hycop 0.2.15) - build 20230731-12",
                     style: CretaFont.headlineLarge,
                   ),
                 ),
