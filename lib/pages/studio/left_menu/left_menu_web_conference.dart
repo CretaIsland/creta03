ef3f2c2cbbece387d1f6d4505345ba4ab5e1346a
Hnarae <nr.han@sqisoft.com>
2023-08-01 16:16:19 +0900

    20230801-01-nr.han fix webrtc connect time

--- lib/pages/studio/left_menu/left_menu_web_conference.dart
+++ lib/pages/studio/left_menu/left_menu_web_conference.dart
@@ -1,10 +1,18 @@
 // stream list
 
 
+import 'package:creta03/pages/login_page.dart';
 import 'package:creta03/pages/studio/left_menu/left_template_mixin.dart';
 import 'package:creta03/pages/studio/left_menu/web_conference/stream/list_remote_stream.dart';
 import 'package:creta03/pages/studio/left_menu/web_conference/stream/local_stream.dart';
 import 'package:flutter/material.dart';
+import 'package:hycop/hycop/account/account_manager.dart';
+import 'package:hycop/hycop/webrtc/media_devices/media_devices_data.dart';
+import 'package:hycop/hycop/webrtc/peers/peers_data.dart';
+import 'package:hycop/hycop/webrtc/producers/producers_data.dart';
+import 'package:hycop/hycop/webrtc/webrtc_client.dart';
+
+import '../book_main_page.dart';
 
 class LeftMenuWebConference extends StatefulWidget {
   final double maxHeight;
@@ -20,6 +28,30 @@ class _LeftMenuWebConferenceState extends State<LeftMenuWebConference> with Left
   void initState() {
     super.initState();
     initMixin();
+    //for webRTC
+    connectServer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    webRTCClient!.close();
+    webRTCClient = null;
+  }
+
+  Future<void> connectServer() async {
+    //for webRTC
+    mediaDeviceDataHolder = MediaDeviceData();
+    peersDataHolder = PeersData();
+    producerDataHolder = ProducerData();
+    await mediaDeviceDataHolder!.loadMediaDevice();
+    webRTCClient = WebRTCClient(
+      roomId: BookMainPage.selectedMid,
+      peerId: AccountManager.currentLoginUser.email,
+      serverUrl: LoginPage.enterpriseHolder!.enterpriseModel!.webrtcUrl,
+      peerName: LoginPage.userPropertyManagerHolder!.userPropertyModel!.nickname
+    );
+    webRTCClient!.connectSocket();
   }
   
   @override
