43477ae0af3da18c9f0472fb3ed88bf0eb703fc7
Hnarae <nr.han@sqisoft.com>
2023-10-20 14:51:55 +0900

    20231020-02-nr.han fix path modify script

--- src_modify/modify.sh
+++ src_modify/modify.sh
@@ -10,13 +10,13 @@
 ###### skpark modify ########
 
 #skpark modify 3/6
-sudo cp src_modify/r_dotted_line_border.daxt $PUB_DEV/r_dotted_line_border-0.1.1/lib/r_dotted_line_border.dart
+sudo cp ./r_dotted_line_border.daxt $PUB_DEV/r_dotted_line_border-0.1.1/lib/r_dotted_line_border.dart
 
 #skpark modify 3/16
-sudo cp src_modify/tag_editor.daxt $PUB_DEV/material_tag_editor-0.1.2/lib/tag_editor.dart
+sudo cp ./tag_editor.daxt $PUB_DEV/material_tag_editor-0.1.2/lib/tag_editor.dart
 
 #skpark modify 4/24
-sudo cp src_modify/adaptive_scrollbar.daxt $PUB_DEV/adaptive_scrollbar-2.1.2/lib/adaptive_scrollbar.dart
+sudo cp ./adaptive_scrollbar.daxt $PUB_DEV/adaptive_scrollbar-2.1.2/lib/adaptive_scrollbar.dart
 
 #skpark modify 6/29
 #copy src_modify\auto_size_text.daxt    C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\auto_size_text-3.0.0\lib\src\auto_size_text.dart
@@ -27,15 +27,15 @@ sudo cp src_modify/adaptive_scrollbar.daxt $PUB_DEV/adaptive_scrollbar-2.1.2/lib
 #copy src_modify\toolbar_widget.daxt    C:\Users\Mai Dang\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart
 
 #skpark modify 7/03
-sudo cp src_modify/neonpen.daxt $PUB_DEV/neonpen-2.0.5/lib/neonpen.dart
+sudo cp ./neonpen.daxt $PUB_DEV/neonpen-2.0.5/lib/neonpen.dart
 
 #skpark modify 7/06
-sudo cp src_modify/weather_thunder_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_thunder_bg.dart
-sudo cp src_modify/weather_rain_snow_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_rain_snow_bg.dart
-sudo cp src_modify/weather_night_star_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_night_star_bg.dart
-sudo cp src_modify/weather_cloud_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_cloud_bg.dart
-sudo cp src_modify/weather_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_bg.dart
-sudo cp src_modify/print_utils.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/utils/print_utils.dart
+sudo cp ./weather_thunder_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_thunder_bg.dart
+sudo cp ./weather_rain_snow_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_rain_snow_bg.dart
+sudo cp ./weather_night_star_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_night_star_bg.dart
+sudo cp ./weather_cloud_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_cloud_bg.dart
+sudo cp ./weather_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_bg.dart
+sudo cp ./print_utils.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/utils/print_utils.dart
 
 #skpark modify 7/7
 #copy src_modify\floating_toolbar.daxt           C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\floating_toolbar.dart
@@ -53,34 +53,34 @@ sudo cp src_modify/print_utils.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.
 #copy summernote-lite.min.jsxt c:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.js
 
 #nr.han modify 9/12
-sudo cp src_modify/html_editor_enhanced/summernote-lite.min.jsxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.js
-sudo cp src_modify/html_editor_enhanced/html_editor_widget_web.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/html_editor_widget_web.dart
-sudo cp src_modify/html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
-sudo cp src_modify/html_editor_enhanced/options.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/options.dart
+sudo cp ./html_editor_enhanced/summernote-lite.min.jsxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.js
+sudo cp ./html_editor_enhanced/html_editor_widget_web.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/html_editor_widget_web.dart
+sudo cp ./html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
+sudo cp ./html_editor_enhanced/options.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/options.dart
 
 #nr.han modify 9/15
 #※ c:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font 폴더에 폰트 파일을 추가해줘야함.
 #※ creta03의 fonts 폴더에 있는 모든 폰트 폴더를 복사 후 위에 경로에 붙여넣기 해주기
-sudo cp src_modify/html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
-sudo cp src_modify/html_editor_enhanced/pubspec.yaxl $PUB_DEV/html_editor_enhanced-2.5.1/pubspec.yaml
-sudo cp src_modify/html_editor_enhanced/summernote-lite.min.cxs $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.css
+sudo cp ./html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
+sudo cp ./html_editor_enhanced/pubspec.yaxl $PUB_DEV/html_editor_enhanced-2.5.1/pubspec.yaml
+sudo cp ./html_editor_enhanced/summernote-lite.min.cxs $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.css
 
 #nr.han modify 9/18
 #copy src_modify\constants.daxt D:\Flutter\src\flutter\packages\flutter\lib\src\material\constants.dart
 
 #seventhstone 23/10/10
-sudo cp src_modify/snowflake_model.daxt $PUB_DEV/snowfall-0.0.2/lib/snowfall/snowflake_model.dart
-sudo cp src_modify/snowfall_pubspec.yaml $PUB_DEV/snowfall-0.0.2/pubspec.yaml
+sudo cp ./snowflake_model.daxt $PUB_DEV/snowfall-0.0.2/lib/snowfall/snowflake_model.dart
+sudo cp ./snowfall_pubspec.yaml $PUB_DEV/snowfall-0.0.2/pubspec.yaml
 
 
 #skpark modify 23/10/10
-sudo cp src_modify/CustomRadioButton.daxt $PUB_DEV/custom_radio_grouped_button-2.2.1/lib/CustomButtons/CustomRadioButton.dart
+sudo cp ./CustomRadioButton.daxt $PUB_DEV/custom_radio_grouped_button-2.2.1/lib/CustomButtons/CustomRadioButton.dart
 
 #seventhstone 23/10/10
-sudo cp src_modify/html_editor_enhanced/utils.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/utils.dart
+sudo cp ./html_editor_enhanced/utils.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/utils.dart
 
 #skpark modify 23/10/17
-sudo cp src_modify/video_player.daxt $PUB_DEV/video_player-2.7.2/lib/video_player.dart
+sudo cp ./video_player.daxt $PUB_DEV/video_player-2.7.2/lib/video_player.dart
 
 #skpark modify 23/10/19
-sudo cp src_modifyjust_audio.daxt $PUB_DEV/just_audio-0.9.35/lib/just_audio.dart
+sudo cp ./just_audio.daxt $PUB_DEV/just_audio-0.9.35/lib/just_audio.dart
