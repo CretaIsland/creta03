bc0b19ccf2722f1a2a1b9088c889f718d8f07bfe
Hnarae <nr.han@sqisoft.com>
2023-10-20 14:46:50 +0900

    20231020-01-nr.han add modify script for linux (devcreta.com)

---
+++ src_modify/modify.sh
@@ -0,0 +1,86 @@
+#cp   C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\hycop-0.0.6\lib\hycop\database\appwrite_database.dart src_modify\appwrite_database.daxt
+#cp   C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\hycop-0.0.6\lib\hycop\database\firebase_database.dart src_modify\firebase_database.daxt
+#cp   C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\hycop-0.0.6\lib\hycop\database\abs_database.dart src_modify\abs_database.daxt
+
+#copy  C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\material_tag_editor-0.1.2\lib\tag_editor.dart src_modify\tag_editor.daxt
+#copy C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\custom_radio_grouped_button-2.2.0\lib\CustomButtons\CustomRadioButton.dart src_modify\CustomRadioButton.daxt
+#copy C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\r_dotted_line_border-0.1.1\lib\r_dotted_line_border.dart  src_modify\r_dotted_line_border.daxt
+#copy  C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\video_player-2.6.1\lib\video_player.dart src_modify\video_player.daxt
+
+###### skpark modify ########
+
+#skpark modify 3/6
+sudo cp src_modify/r_dotted_line_border.daxt $PUB_DEV/r_dotted_line_border-0.1.1/lib/r_dotted_line_border.dart
+
+#skpark modify 3/16
+sudo cp src_modify/tag_editor.daxt $PUB_DEV/material_tag_editor-0.1.2/lib/tag_editor.dart
+
+#skpark modify 4/24
+sudo cp src_modify/adaptive_scrollbar.daxt $PUB_DEV/adaptive_scrollbar-2.1.2/lib/adaptive_scrollbar.dart
+
+#skpark modify 6/29
+#copy src_modify\auto_size_text.daxt    C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\auto_size_text-3.0.0\lib\src\auto_size_text.dart
+
+
+#mai modify 7/5
+#copy src_modify\callbacks.daxt    C:\Users\Mai Dang\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\utils\callbacks.dart
+#copy src_modify\toolbar_widget.daxt    C:\Users\Mai Dang\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart
+
+#skpark modify 7/03
+sudo cp src_modify/neonpen.daxt $PUB_DEV/neonpen-2.0.5/lib/neonpen.dart
+
+#skpark modify 7/06
+sudo cp src_modify/weather_thunder_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_thunder_bg.dart
+sudo cp src_modify/weather_rain_snow_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_rain_snow_bg.dart
+sudo cp src_modify/weather_night_star_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_night_star_bg.dart
+sudo cp src_modify/weather_cloud_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_cloud_bg.dart
+sudo cp src_modify/weather_bg.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/bg/weather_bg.dart
+sudo cp src_modify/print_utils.daxt $PUB_DEV/flutter_weather_bg_null_safety-1.0.0/lib/utils/print_utils.dart
+
+#skpark modify 7/7
+#copy src_modify\floating_toolbar.daxt           C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\floating_toolbar.dart
+#copy src_modify\editor.daxt                     C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\editor_component\service\editor.dart
+#copy src_modify\keyboard_service_widget.daxt    C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\editor_component\service\keyboard_service_widget.dart
+#skpark modify 7/9
+#copy src_modify\color_picker.daxt C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\items\color\color_picker.dart
+#copy src_modify\text_color_toolbar_item.daxt C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\items\color\text_color_toolbar_item.dart
+#copy src_modify\highlight_color_toolbar_item.daxt C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\items\color\highlight_color_toolbar_item.dart
+
+#skpark modify 7/12
+#copy src_modify\color_menu.daxt C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\appflowy_editor-1.1.0\lib\src\editor\toolbar\desktop\items\color\color_menu.dart
+
+#skpark modify 9/5
+#copy summernote-lite.min.jsxt c:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.js
+
+#nr.han modify 9/12
+sudo cp src_modify/html_editor_enhanced/summernote-lite.min.jsxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.js
+sudo cp src_modify/html_editor_enhanced/html_editor_widget_web.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/html_editor_widget_web.dart
+sudo cp src_modify/html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
+sudo cp src_modify/html_editor_enhanced/options.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/options.dart
+
+#nr.han modify 9/15
+#※ c:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font 폴더에 폰트 파일을 추가해줘야함.
+#※ creta03의 fonts 폴더에 있는 모든 폰트 폴더를 복사 후 위에 경로에 붙여넣기 해주기
+sudo cp src_modify/html_editor_enhanced/toolbar_widget.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/src/widgets/toolbar_widget.dart
+sudo cp src_modify/html_editor_enhanced/pubspec.yaxl $PUB_DEV/html_editor_enhanced-2.5.1/pubspec.yaml
+sudo cp src_modify/html_editor_enhanced/summernote-lite.min.cxs $PUB_DEV/html_editor_enhanced-2.5.1/lib/assets/summernote-lite.min.css
+
+#nr.han modify 9/18
+#copy src_modify\constants.daxt D:\Flutter\src\flutter\packages\flutter\lib\src\material\constants.dart
+
+#seventhstone 23/10/10
+sudo cp src_modify/snowflake_model.daxt $PUB_DEV/snowfall-0.0.2/lib/snowfall/snowflake_model.dart
+sudo cp src_modify/snowfall_pubspec.yaml $PUB_DEV/snowfall-0.0.2/pubspec.yaml
+
+
+#skpark modify 23/10/10
+sudo cp src_modify/CustomRadioButton.daxt $PUB_DEV/custom_radio_grouped_button-2.2.1/lib/CustomButtons/CustomRadioButton.dart
+
+#seventhstone 23/10/10
+sudo cp src_modify/html_editor_enhanced/utils.daxt $PUB_DEV/html_editor_enhanced-2.5.1/lib/utils/utils.dart
+
+#skpark modify 23/10/17
+sudo cp src_modify/video_player.daxt $PUB_DEV/video_player-2.7.2/lib/video_player.dart
+
+#skpark modify 23/10/19
+sudo cp src_modifyjust_audio.daxt $PUB_DEV/just_audio-0.9.35/lib/just_audio.dart
