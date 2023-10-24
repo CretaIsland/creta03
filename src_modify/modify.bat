df47452f58e79e3180bbb66f8ad306ad51f8529e
skpark33 <skpark@sqisoft.com>
2023-10-24 20:18:14 +0900

    20231024-03-skpark

--- src_modify/modify.bat
+++ src_modify/modify.bat
@@ -89,3 +89,6 @@ copy src_modify\just_audio.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\just
 #mdang modify 23/10/20
 copy src_modify\radio_button.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_button.dart"
 copy src_modify\radio_group.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_group.dart"
+
+#skpark modify 23/10/24
+copy src_modify\just_audio.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\just_audio-0.9.35\lib\just_audio.dart"
