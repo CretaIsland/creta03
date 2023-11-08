60d2716cb82b421023067e807f622e7d8f2b6559
skpark33 <skpark@sqisoft.com>
2023-11-08 16:46:51 +0900

    20231108-05-skpark

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -42,7 +42,7 @@ class _IntroPageState extends State<IntroPage> {
     "0.3.13",
     "0.3.12",
   ];
-  final String hycopVersion = "0.3.21";
+  final String hycopVersion = "0.3.22";
   final String buildNumber = "20231108-16(appwrite)";
 
   @override
