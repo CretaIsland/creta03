cd969c4709fb1f374f41bde6d02ad095b93c71e9
seventhstone@sqisoft.com <seventhstone@sqisoft.com>
2024-01-26 19:04:14 +0900

    20240126-01-seventhstone - change login-dialog

--- lib/design_system/dialog/creta_dialog.dart
+++ lib/design_system/dialog/creta_dialog.dart
@@ -26,35 +26,103 @@ class CretaDialog extends StatelessWidget {
     return Dialog(
       backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-      child: SizedBox(
-        width: width,
-        height: height,
-        child: Column(
-          crossAxisAlignment: crossAxisAlign,
-          children: [
-            Padding(
-                padding: const EdgeInsets.only(left: 16.0, top: 13.0, right: 12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      title,
-                      style: CretaFont.titleMedium,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: Column(
+            crossAxisAlignment: crossAxisAlign,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.only(left: 16.0, top: 13.0, right: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        title,
+                        style: CretaFont.titleMedium,
+                      ),
+                      BTN.fill_gray_i_s(icon: Icons.close, onPressed: () => Navigator.of(context).pop())
+                    ],
+                  )),
+              (hideTopSplitLine)
+                  ? const SizedBox.shrink()
+                  : Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Container(
+                        width: width,
+                        height: 2.0,
+                        color: CretaColor.text[100], //Colors.grey.shade200,
+                      ),
                     ),
-                    BTN.fill_gray_i_s(icon: Icons.close, onPressed: () => Navigator.of(context).pop())
-                  ],
-                )),
-            (hideTopSplitLine)
-                ? const SizedBox.shrink()
-                : Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: Container(
-                      width: width,
-                      height: 2.0,
-                      color: CretaColor.text[100], //Colors.grey.shade200,
+              content
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CretaStackDialog extends StatelessWidget {
+  const CretaStackDialog({
+    super.key,
+    required this.width,
+    required this.height,
+    this.title = '',
+    this.hideTopSplitLine = true,
+    required this.content,
+  });
+
+  final double width;
+  final double height;
+  final String title;
+  final bool hideTopSplitLine;
+  final Widget content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Stack(
+          children: [
+            content,
+            SizedBox(
+              width: width,
+              height: height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, top: 13.0, right: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          title,
+                          style: CretaFont.titleMedium,
+                        ),
+                        BTN.fill_gray_i_s(icon: Icons.close, onPressed: () => Navigator.of(context).pop())
+                      ],
                     ),
                   ),
-            content
+                  (hideTopSplitLine)
+                      ? const SizedBox.shrink()
+                      : Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Container(
+                            width: width,
+                            height: 2.0,
+                            color: CretaColor.text[100], //Colors.grey.shade200,
+                          ),
+                        ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
